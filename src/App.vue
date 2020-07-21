737d2d481d777c9674298ce026dd98ce5984e261
Gabriel Cesario <cesario203@outlook.com>
2020-07-20 23:51:31 -0300

    Finalizando de estilizar o componente de perfil e contato

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
   }
 
   #app{
--- src/components/template/SideProfile.vue
+++ src/components/template/SideProfile.vue
@@ -1,17 +1,65 @@
 <template>
   <div class="side-profile">
-      <h1>Eu fico na esquerda</h1>
+    <h2>Gabriel Cesario</h2>
+    <h2>Desenvolvedor web</h2>
+    <h3>Aspirante em desenvolvimento web e eterno aprendiz na jornada da vida</h3>
+    <div class="social-links">
+      <li>
+        <a href="https://www.linkedin.com/in/gabriel-cesario-04784ab2/" target="_blank">
+          <i class="fa fa-linkedin fa-3x"></i>
+        </a>
+      </li>
+      <li>
+        <a href="mailto:cesario203@outlook.com">
+          <i class="fa fa-envelope fa-3x"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/gcesario203" target="_blank">
+          <i class="fa fa-github fa-3x"></i>
+        </a>
+      </li>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name:'SideProfile'
-}
+  name: "SideProfile"
+};
 </script>
 
 <style>
-    .side-profile{
-        grid-area: SideProfile;
-    }
+.side-profile {
+  padding: 30px;
+  color: white;
+  text-align: center;
+  background-color: rgb(46, 44, 44);
+  font-weight: 100;
+  grid-area: SideProfile;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.side-profile .social-links {
+  display: flex;
+  list-style: none;
+  width: 100%;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.side-profile .social-links a{
+    color: white;
+    transition: 500ms;
+}
+
+.side-profile .social-links a:hover,
+.side-profile .social-links a:focus{
+    color: rgb(233, 229, 0);
+}
 </style>
